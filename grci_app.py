2261390e0b19b5ef4783fc045caaf9ef88059ed4
mostafa11200 <mostafagaber112000@gmail.com>
2024-11-07 20:20:20 +0200

    day1

--- grci_app.py
+++ grci_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-st.title("🎈 My new app1")
+st.title("GRCi")
 st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+    "Home Page."
 )
