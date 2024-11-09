8e4b17a6dc229febf59dd62115c22905addf3dd2
mostafa11200 <mostafagaber112000@gmail.com>
2024-11-09 11:09:23 +0200

    day2: menu items

--- grci_app.py
+++ grci_app.py
@@ -1,6 +1,55 @@
-import streamlit as st # type: ignore
+import streamlit as st
 
-st.title("GRCi")
-st.write(
-    "Home Page."
+from streamlit_option_menu import option_menu
+
+
+import home, account, about, pioneer
+st.set_page_config(
+        page_title="GRCi",
 )
+
+
+
+class MultiApp:
+
+    def __init__(self):
+        self.apps = []
+
+    def add_app(self, title, func):
+
+        self.apps.append({
+            "title": title,
+            "function": function
+        })
+
+    def run():
+        # app = st.sidebar(
+        with st.sidebar:        
+            app = option_menu(
+                menu_title='GRCi ',
+                options=['Home','Account','About','Pioneer'],
+                icons=['house-fill','person-circle','trophy-fill','chat-fill','info-circle-fill'],
+                menu_icon='chat-text-fill',
+                default_index=1,
+                styles={
+                    "container": {"padding": "5!important","background-color":'black'},
+        "icon": {"color": "white", "font-size": "23px"}, 
+        "nav-link": {"color":"white","font-size": "20px", "text-align": "left", "margin":"0px", "--hover-color": "blue"},
+        "nav-link-selected": {"background-color": "#02ab21"},}
+                
+                )
+
+        
+        if app == "Home":
+            home.app()
+        if app == "Account":
+            account.app()    
+        if app == 'About':
+            about.app()    
+        if app=='Pioneer':
+            pioneer.app()    
+             
+          
+             
+    run()            
+         
